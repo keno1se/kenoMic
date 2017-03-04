c074f49dc004530d32d41c07aefb997258127e60
Kenneth Nordman <keno1se@yahoo.se>
2017-03-04 14:09:38 +0100

    Add files via upload

---
+++ js/mouseScript.js
@@ -0,0 +1,207 @@
+﻿    
+/***********************/
+/*** scriptMouseOver ***/
+/***********************/
+
+/* into variable x get text from p id="second" - this is second, comes at <br /> mouseover */
+/* copy over that text to p is="first" which writes - this is first, comes at <br /> reload*/
+
+function scriptMouseOverPic1() {
+    var x = document.getElementById("textPic1").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic2").style.opacity = "0.5";
+    document.getElementById("pic3").style.opacity = "0.5";
+    document.getElementById("pic4").style.opacity = "0.5";
+    document.getElementById("pic5").style.opacity = "0.5";
+    document.getElementById("pic6").style.opacity = "0.5";
+    document.getElementById("pic7").style.opacity = "0.5";
+    document.getElementById("pic8").style.opacity = "0.5";
+    document.getElementById("pic9").style.opacity = "0.5";
+
+
+    //document.getElementById("first").style.background = "orange"; this works
+    //document.getElementById("first").style.background = "hsl(50, 100%, 80%)"; bit not this?
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+function scriptMouseOverPic2() {
+    var x = document.getElementById("textPic2").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic1").style.opacity = "0.5";
+
+    document.getElementById("pic3").style.opacity = "0.5";
+    document.getElementById("pic4").style.opacity = "0.5";
+    document.getElementById("pic5").style.opacity = "0.5";
+    document.getElementById("pic6").style.opacity = "0.5";
+    document.getElementById("pic7").style.opacity = "0.5";
+    document.getElementById("pic8").style.opacity = "0.5";
+    document.getElementById("pic9").style.opacity = "0.5";
+
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+function scriptMouseOverPic3() {
+    var x = document.getElementById("textPic3").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic1").style.opacity = "0.5";
+    document.getElementById("pic2").style.opacity = "0.5";
+
+    document.getElementById("pic4").style.opacity = "0.5";
+    document.getElementById("pic5").style.opacity = "0.5";
+    document.getElementById("pic6").style.opacity = "0.5";
+    document.getElementById("pic7").style.opacity = "0.5";
+    document.getElementById("pic8").style.opacity = "0.5";
+    document.getElementById("pic9").style.opacity = "0.5";
+
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+function scriptMouseOverPic4() {
+    var x = document.getElementById("textPic4").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic1").style.opacity = "0.5";
+    document.getElementById("pic2").style.opacity = "0.5";
+    document.getElementById("pic3").style.opacity = "0.5";
+
+    document.getElementById("pic5").style.opacity = "0.5";
+    document.getElementById("pic6").style.opacity = "0.5";
+    document.getElementById("pic7").style.opacity = "0.5";
+    document.getElementById("pic8").style.opacity = "0.5";
+    document.getElementById("pic9").style.opacity = "0.5";
+
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+function scriptMouseOverPic5() {
+    var x = document.getElementById("textPic5").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic1").style.opacity = "0.5";
+    document.getElementById("pic2").style.opacity = "0.5";
+    document.getElementById("pic3").style.opacity = "0.5";
+    document.getElementById("pic4").style.opacity = "0.5";
+
+    document.getElementById("pic6").style.opacity = "0.5";
+    document.getElementById("pic7").style.opacity = "0.5";
+    document.getElementById("pic8").style.opacity = "0.5";
+    document.getElementById("pic9").style.opacity = "0.5";
+
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+function scriptMouseOverPic6() {
+    var x = document.getElementById("textPic6").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic1").style.opacity = "0.5";
+    document.getElementById("pic2").style.opacity = "0.5";
+    document.getElementById("pic3").style.opacity = "0.5";
+    document.getElementById("pic4").style.opacity = "0.5";
+    document.getElementById("pic5").style.opacity = "0.5";
+
+    document.getElementById("pic7").style.opacity = "0.5";
+    document.getElementById("pic8").style.opacity = "0.5";
+    document.getElementById("pic9").style.opacity = "0.5";
+
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+function scriptMouseOverPic7() {
+    var x = document.getElementById("textPic7").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic1").style.opacity = "0.5";
+    document.getElementById("pic2").style.opacity = "0.5";
+    document.getElementById("pic3").style.opacity = "0.5";
+    document.getElementById("pic4").style.opacity = "0.5";
+    document.getElementById("pic5").style.opacity = "0.5";
+    document.getElementById("pic6").style.opacity = "0.5";
+
+    document.getElementById("pic8").style.opacity = "0.5";
+    document.getElementById("pic9").style.opacity = "0.5";
+
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+function scriptMouseOverPic8() {
+    var x = document.getElementById("textPic8").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic1").style.opacity = "0.5";
+    document.getElementById("pic2").style.opacity = "0.5";
+    document.getElementById("pic3").style.opacity = "0.5";
+    document.getElementById("pic4").style.opacity = "0.5";
+    document.getElementById("pic5").style.opacity = "0.5";
+    document.getElementById("pic6").style.opacity = "0.5";
+    document.getElementById("pic7").style.opacity = "0.5";
+
+    document.getElementById("pic9").style.opacity = "0.5";
+
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+function scriptMouseOverPic9() {
+    var x = document.getElementById("textPic9").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+    document.getElementById("pic1").style.opacity = "0.5";
+    document.getElementById("pic2").style.opacity = "0.5";
+    document.getElementById("pic3").style.opacity = "0.5";
+    document.getElementById("pic4").style.opacity = "0.5";
+    document.getElementById("pic5").style.opacity = "0.5";
+    document.getElementById("pic6").style.opacity = "0.5";
+    document.getElementById("pic7").style.opacity = "0.5";
+    document.getElementById("pic8").style.opacity = "0.5";
+
+    //document.getElementById("first").style.color = "red";
+    document.getElementById("first").style.border = "2px solid orange";
+
+}
+
+
+/**********************/
+/*** scriptMouseOut ***/
+/**********************/
+
+/* into variable x get text from p id="third" - this is third, comes at <br /> mouseout */
+/* copy over that text to p is="first" which writes - this is first, comes at <br /> reload*/
+
+function scriptMouseOutPic() {
+    var x = document.getElementById("last").innerHTML;
+    document.getElementById("first").innerHTML = x;
+
+//    document.getElementById("first").style.color = "blue";
+
+    document.getElementById("pic1").style.opacity = "1.0";
+    document.getElementById("pic2").style.opacity = "1.0";
+    document.getElementById("pic3").style.opacity = "1.0";
+    document.getElementById("pic4").style.opacity = "1.0";
+    document.getElementById("pic5").style.opacity = "1.0";
+    document.getElementById("pic6").style.opacity = "1.0";
+    document.getElementById("pic7").style.opacity = "1.0";
+    document.getElementById("pic8").style.opacity = "1.0";
+    document.getElementById("pic9").style.opacity = "1.0";
+    
+    document.getElementById("first").style.border = "2px solid black";
+    //document.getElementById("first").style.border = "3px solid red";
+
+
+
+}
+
+
+/* (1-reload)"first" text is visible in the DOM at reloade  */
+/* (2-mouseover)"second" text is hidden but it's content writes over the visible "first" and is now visible  */
+/* (3-mouseout)"third" text is hidden but it's content writes over the visible "first" and is now visible   */
